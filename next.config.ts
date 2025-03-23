9eac688001c7b7e67f446ec070b6ff0d4ae6a5ff
froide-inoueyuji <yuji-inoue@froide.co.jp>
2025-03-23 23:00:39 +0900

    設定を追加

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
-  outputFileTracingRoot: process.cwd(), // experimentalから移動
+	output: "standalone",
+	outputFileTracingRoot: process.cwd(),
 };
 
 export default nextConfig;
